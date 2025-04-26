d2c69a25e848cd319e2375f8ed2020adb7efaea9
Maxim Dashkovskiy <maxdashk3@gmail.com>
2025-04-26 20:08:35 +0300

    added lab 8

--- create_index.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["performance_test"]
-collection = db["sales"]
-
-collection.create_index([("category", 1)])  # Створення індексу на поле category
-print("Index on 'category' field created successfully.")
